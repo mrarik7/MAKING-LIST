03fda81b77bb60e476ba90f062dc1d89d99afd3b
mrarik7 <arikff007@gmail.com>
2026-02-02 19:03:57 +0600

    Add files via upload

---
+++ bazarlist.py
@@ -0,0 +1,36 @@
+import streamlit as st
+
+col1,col2,col3 = st.columns(3)
+with col1:
+    st.header('Make Your List📙')
+with col3:
+    if st.button('ℹ️ info'):
+        st.text('1.write one by one\n2.write "end"when you finish\n3.take a screanshot for later usage')
+if 'msg' not in st.session_state:
+    st.session_state.msg = []
+
+msg = st.chat_input('write here...')
+if msg:
+    st.session_state.msg.append(msg)
+
+for i in st.session_state.msg:
+    with st.chat_message('ai'):
+        st.write(i)
+
+
+if msg == 'end':
+    st.subheader('your list is ready💁')
+    st.write(st.session_state.msg)
+    st.write ('⚠️take a screanshot so that you have a copy for later uses:)')
+    
+
+
+
+
+
+
+
+
+
+
+
